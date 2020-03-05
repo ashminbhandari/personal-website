783e107619c6faa0232edb69a7ee00c2ff20ffb3
Ashmin Bhandari <abhandar@ramapo.edu>
2020-03-05 01:52:59 -0500

    Front page creation. Need to set up routing to two separate sites: programmer/photographer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
+import Typing from 'react-typing-animation';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <div className="main-content">
+                <Typing className="text-design" cursorClassName="text-design" speed={150}>
+                    <span>if (hereFor === coder) &#123; </span>
+                </Typing>
+                <div>
+                    <i className="fa fa-code btn"/>
+                </div>
+                <Typing className="text-design" cursorClassName="text-design" speed = {150} startDelay={4200}>
+                    <span>}</span>
+                </Typing>
+                <br/>
+                <br/>
+                <Typing className="text-design" cursorClassName="text-design" speed = {150} startDelay={4700}>
+                    <span>else &#123;</span>
+                </Typing>
+                <div>
+                    <i className="fa fa-camera btn"/>
+                </div>
+                <Typing className="text-design" cursorClassName="text-design" speed = {150} startDelay={6000}>
+                    <span>}</span>
+                </Typing>
+            </div>
+        </div>
+    );
 }
 
 export default App;
